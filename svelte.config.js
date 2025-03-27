e73afa01026089653ab647aa68d5a6b97cf91f34
CHOBI <139092470+CHOBI-project@users.noreply.github.com>
2025-03-28 00:45:01 +0900

    :wrench: - deploy options.

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,9 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		prerender: {
+			default: true,
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
@@ -27,7 +30,6 @@ const config = {
 		paths: {
 			base: "/chobi-project"
 		},
-		
 	},
 };
 
